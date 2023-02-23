3d127395ca62c68f55aba67199d70bad440299c0
sama-pratap-reddy <pratap.sama@gmail.com>
2023-02-23 12:21:40 +0530

    calculator implemented

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
-
+  public number1:number =0;
+  public number2:number =0;
+  public Result:number =0;
+sum(){
+  this.Result=this.number1+this.number2
+}
+sub(){
+  this.Result=this.number1-this.number2
+}
+mul(){
+  this.Result=this.number1*this.number2
+}
+div(){
+  this.Result=this.number1/this.number2
+}
 }
