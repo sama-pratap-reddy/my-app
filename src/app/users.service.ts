80a15c6d872fba9042877d913d8f72b68e80e801
sama-pratap-reddy <pratap.sama@gmail.com>
2023-03-06 12:03:36 +0530

    users

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -6,9 +6,23 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UsersService {
+  public URL: string = "https://62abe711bd0e5d29af16f450.mockapi.io/users"
+  constructor(private _httpClient: HttpClient) { }
 
-  constructor(private _httpClient:HttpClient) { }
-  getUsers():Observable<any>{
-    return this._httpClient.get("https://62abe711bd0e5d29af16f450.mockapi.io/users")
+  getUsers(): Observable<any> {
+    return this._httpClient.get(this.URL + "?limit=10&page=1");
+  };
+  getFilteredFUsers(term: string): Observable<any> {
+    return this._httpClient.get(this.URL + "?filter=" + term);
+  };
+
+  getSortedUsers(column: string, order: string): Observable<any> {
+    return this._httpClient.get(this.URL + "?sortBy=" + column + "&order=" + order);
+  };
+  getPageUsers(page: number, limit: number): Observable<any> {
+    return this._httpClient.get(this.URL + "?limit=" + limit + "&page=" + page);
+  };
+  deleteUser(id:string):Observable<any>{
+    return this._httpClient.delete(this.URL+"/"+id);
   }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,6 +8,9 @@ import { UsersService } from '../users.service';
 })
 export class UsersComponent {
   public users:any=[];
+  public term:string="";
+  public column:string=""
+  public order:string=""
 constructor(private _usersService:UsersService){
 _usersService.getUsers().subscribe(
   (data:any)=>{
@@ -18,5 +21,50 @@ _usersService.getUsers().subscribe(
     alert("internal server error");
   }
 )
+};
+filter(){
+
+    this._usersService.getFilteredFUsers(this.term).subscribe(
+      (data:any)=>{
+        this.users = data;
+    
+      },
+      (err:any)=>{
+        alert("internal server error");
+      }
+    )
+}
+sort(){
+  this._usersService.getSortedUsers(this.column,this.order).subscribe(
+    (data:any)=>{
+      this.users = data;
+  
+    },
+    (err:any)=>{
+      alert("internal server error");
+    }
+  )
+}
+page(no:number){
+  this._usersService.getPageUsers(no,10).subscribe(
+    (data:any)=>{
+      this.users = data;
+  
+    },
+    (err:any)=>{
+      alert("internal server error");
+    }
+  )
+}
+delete(id:string){
+  this._usersService.deleteUser(id).subscribe(
+  (data:any)=>{
+    alert("deleted successfully");
+    location.reload();
+  },
+  (err:any)=>{
+    alert("internal serever error");
+  }
+  )
 }
 }
